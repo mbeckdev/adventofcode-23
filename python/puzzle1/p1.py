36dbfeec6f67ce01be73cd1e8c0c59e3983a5813
mbeckdev <mbeckdev42@gmail.com>
2023-12-03 18:42:10 -0700

    Add soln to part 1 puzzle 1

--- python/puzzle1/p1.py
+++ python/puzzle1/p1.py
@@ -1,57 +1,35 @@
 import os
-# print("Current working directory:", os.getcwd())
 puzzleNo = "1"
+partNo = "1"
 
-testPathStringsList = ["python\\puzzle", puzzleNo, "\\p", puzzleNo, "test.txt"]
+testPathStringsList = ["python\\puzzle", puzzleNo, "\\p", puzzleNo, "test", partNo, ".txt"]
 testPath = "".join(testPathStringsList)
-# print('testPath = ',testPath)
-realPathStringsList = ["python\\puzzle", puzzleNo, "\\p", puzzleNo, "input.txt"]
+realPathStringsList = ["python\\puzzle", puzzleNo, "\\p", puzzleNo, "input", partNo, ".txt"]
 realPath = "".join(realPathStringsList)
-# print('realPath = ',realPath)
+# print(realPath)
 
-# all these work:   - the r before the string does the same as  \\   \\ in a normal string
-# myfilepath = r"python\puzzle1\p1test.txt"
-# myfilepath = os.path.abspath(r"python\puzzle1\p1test.txt")
-# myfilepath = os.path.abspath(r"python/puzzle1/p1test.txt")
-
-myfile = open(testPath, "r")
-# myfile = open(realPath, "r")
-# myfile = open(myfilepath, "r")
-# myfile = open("python\puzzle1\p1input.txt", "r")
-
-# print('**********************')
-# print('***start***')
-# # Open the file in read mode ('r')
-# with open('p1input.txt', 'r') as file:
-#     # Read all lines into a list
-#     lines = file.readlines()
-
-# # Print each line
-# for line in lines:
-#     print(line)
-#     # print(line.strip())
-
-
-
-print(myfile.read())
-print('***********end**********sss****')
-
-
-# print(myfile.read())
-# myfile = open("blah.txt", "r")
-# myfile = open("C:\Users\Miche\dev\workspace\adventofcode-23\python\puzzle1\p1input.txt", "r")
-
-# # Specify the path to your .txt file
-# file_path = 'p1input.txt'
-
-# # Open the file in read mode ('r')
-# with open(file_path, 'r') as file:
-#     # Read the entire contents of the file into a string
-#     file_content = file.read()
-
-#     # Alternatively, you can read the file line by line
-#     # for line in file:
-#     #     print(line.strip())
-
-# # Now you can use the file content as needed
-# print(file_content)
+print('********** start ********** start *******')
+# with open(testPath, "r") as file:
+with open(realPath, "r") as file:
+    lines = file.readlines()
+    # print(lines)
+    ans = 0
+    for line in lines:
+        # print(line)   # line is like 1abc2df
+        # print(line.strip())    # the strip gets rid of spaces and crlf 's  line is ilke 3sfd1fj
+        first = "empty"
+        last = "empty"
+        for char in line:
+            if char.isdigit() and first == "empty":
+                first = char
+                # print(char)
+            if char.isdigit():
+                last = char
+                # print(char)
+        num = int(first + last)
+        # print("num = ")
+        # print(num)
+        ans += num
+    #     # print(line.find([0-9]))
+print('***********end**********end*******')
+print(ans)
